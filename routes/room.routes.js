ad701dc899337f104939e97e1a924ed3fe3c54b6
Aayush <118432407+Aayush-khadka@users.noreply.github.com>
2025-08-03 13:46:03 +0545

    fix: add status route to room.routes.js for quiz state checking

--- routes/room.routes.js
+++ routes/room.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createRoom, roomLobby } from "../controllers/room.controller.js";
+import {
+  createRoom,
+  isQuizStarted,
+  roomLobby,
+} from "../controllers/room.controller.js";
 
 const router = express.Router();
 
 router.route("/room/create").post(createRoom);
 
 router.route("/room/lobby/:roomcode").get(roomLobby);
+
+router.route("/room/status/:roomcode").get(isQuizStarted);
 
 export default router;
