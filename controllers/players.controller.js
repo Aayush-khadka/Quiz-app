8a1bb2edea1e22a3a82339916bc21e04c3961df7
Aayush <118432407+Aayush-khadka@users.noreply.github.com>
2025-08-11 14:57:45 +0545

    fix: improve error messages and response structure in joinLobby function

--- controllers/players.controller.js
+++ controllers/players.controller.js
@@ -10,44 +10,52 @@ export const joinLobby = asynchandler(async (req, res) => {
   const { name } = req.body;
 
   if (!roomcode || !name) {
-    throw new ApiError(400, "Room Code or the player's Name is Missing!!!");
+    throw new ApiError(400, "Room code or the player's name is missing!!!");
   }
 
   const doesRoomExist = await Questions.findOne({ room_code: roomcode });
-
   if (!doesRoomExist) {
-    throw new ApiError(404, "Room with that Code Not Found!!");
+    throw new ApiError(404, "Room with that code not found!!");
   }
+
+  if (doesRoomExist.quizStarted == true) {
+    throw new ApiError(
+      409,
+      "The quiz already started wait for host to start a new game"
+    );
+  }
+
   const samePlayerName = await Players.findOne({
     playerName: name,
     room_code: roomcode,
   });
-
-  if (doesRoomExist.quizStarted == true) {
-    throw new ApiError(
-      500,
-      "The Quiz Already Started Wait for Host to Start a new game!!!"
-    );
-  }
-
   if (samePlayerName) {
-    throw new ApiError(400, "Player with this name is Already in Lobby!!!");
+    throw new ApiError(400, "Player with this name is already in lobby!!!");
   }
 
   const uniqueId = uuidv4();
-
   const insertPlayer = await Players.create({
     room_code: roomcode,
     playerName: name,
     userId: uniqueId,
   });
+
   if (!insertPlayer) {
-    throw new ApiError(500, "Failed To join the Lobby!!");
+    throw new ApiError(500, "Failed to join the lobby!!");
   }
 
-  res.status(200).json(new ApiResponse(200, insertPlayer, "Room exists"));
+  res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        room_code: roomcode,
+        userId: uniqueId,
+        playerName: name,
+      },
+      "Successfully joined the room"
+    )
+  );
 });
-
 export const host = asynchandler(async (req, res) => {
   const { roomcode } = req.params;
   console.log(roomcode);
